c40d7e6131e1a17d89aa863e7474f0b6c13b9c75
karcagmate <karcagmate@stud.uni-obuda.hu>
2024-05-18 20:34:03 +0200

    model updated

--- LabelPreparation.py
+++ LabelPreparation.py
@@ -5,15 +5,23 @@ import numpy as np
 
 class LabelPrep:
 
-    def __init__(self,labels_path,video_name) -> None:
+    def __init__(self,labels_path) -> None:
         self.labels_path=labels_path
-        self.video_name=video_name
+        #self.video_name=video_name
 
     def prep_label(self):
-        labels_file_path=os.path.join(self.labels_path,f"label_{self.video_name}.txt")
-        labels_df=pd.read_csv(labels_file_path,delim_whitespace=True)
-        labels_df=labels_df.drop(columns=('Frame'),axis=1)
-        return labels_df
+        all_labels=[]
+        labels_path=sorted(os.listdir(self.labels_path))
+        for label_file in labels_path:
+            labels_file_path=os.path.join(self.labels_path,label_file)
+            labels_df=pd.read_csv(labels_file_path,delim_whitespace=True)
+            labels_df=labels_df.drop(columns=['Frame'])
+            all_labels.append(labels_df)
+        combined=pd.concat(all_labels,ignore_index=True)
+        return combined
+
+
+        
 
 import cv2
 import os
@@ -23,15 +31,18 @@ class Train_Test:
 
         
     def into_array(self):
-        file_list=os.listdir(self.file_path)
-        images=[]
-        for file_name in file_list:
-            file=os.path.join(self.file_path,file_name)
-            image=cv2.imread(file)
-            image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-            images.append(image)
-        images=np.array(images)
-        return images
+        all_frames = []
+        for video_folder in sorted(os.listdir(self.file_path)):
+            video_path = os.path.join(self.file_path, video_folder)
+            
+            frame_files = sorted(os.listdir(video_path))
+            for frame_file in frame_files:
+                frame_path = os.path.join(video_path, frame_file)
+                image = cv2.imread(frame_path)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                all_frames.append(image)
+        frames_array = np.array(all_frames)
+        return frames_array
     
     
 
--- ModelPreparation.py
+++ ModelPreparation.py
@@ -14,28 +14,40 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 class Build_model:
-    def __init__(self,train_images,test_images,train_labels,test_labels) -> None:
+    def __init__(self,train_images,validation_images,test_images,train_labels,validation_labels,test_labels) -> None:
         self.train_images=train_images
+        self.validation_images=validation_images
         self.test_images=test_images
         self.train_labels=train_labels
+        self.validation_labels=validation_labels
         self.test_labels=test_labels
-        self.batch_size=64
-        self.epochs=3
-        self.num_classes=8 #len(np.unique(self.train_labels))
+        self.batch_size=40
+        self.epochs=10
+        self.num_classes=7 #len(np.unique(self.train_labels))
 
 
     def normalise_values(self):
         self.train_images=self.train_images.astype('float32')
         self.test_images_images=self.test_images.astype('float32')
         self.train_images=self.train_images.reshape(-1,250,250,1)
+        self.validation_images=self.validation_images.reshape(-1,250,250,1)
         self.test_images=self.test_images.reshape(-1,250,250,1)
-        self.train_labels=to_categorical(self.train_labels) #convert to one hot encoded labels
-        self.test_labels=to_categorical(self.test_labels) #convert to one hot encoded leabels
+        self.train_labels -= 1
+        self.validation_labels -= 1
+        self.test_labels -= 1
+        self.train_labels=self.train_labels.astype(int)
+        self.validation_labels=self.validation_labels.astype(int)
+        self.test_labels=self.test_labels.astype(int)
+        self.train_labels=to_categorical(self.train_labels,num_classes=self.num_classes) #convert to one hot encoded labels
+        self.validation_labels=to_categorical(self.validation_labels,num_classes=self.num_classes) #convert to one hot encoded labels
+        self.test_labels=to_categorical(self.test_labels,num_classes=self.num_classes) #convert to one hot encoded leabels
         #split train images and labels into train and validation
-        train_X,valid_X,train_label,valid_label = train_test_split(self.train_images, self.train_labels, test_size=0.2, random_state=13)
-        train_X=train_X/ 255. #normalise train images
-        valid_X=valid_X/255. #normalise validation images
-        self.test_images=self.test_images/ 255. # normalise test images
+        #train_X,valid_X,train_label,valid_label = train_test_split(self.train_images, self.train_labels, test_size=0.2, random_state=13)
+        train_X=self.train_images #normalise train images
+        valid_X=self.validation_images #normalise validation images
+        #self.test_images=self.test_images/ 255. # normalise test images
+        train_label=self.train_labels
+        valid_label=self.validation_labels
 
         return train_X,valid_X,train_label,valid_label
         #self.train_images=self.train_images/255.0
@@ -45,18 +57,24 @@ class Build_model:
     def build_model(self):
         
         detection_model=Sequential()
-        detection_model.add(Conv2D(32,kernel_size=(3,3),activation='linear',input_shape=(250,250,1),padding='same'))
-        detection_model.add(LeakyReLU(alpha=0.1))
+        #first layer
+        detection_model.add(Conv2D(32,kernel_size=(5,5),activation='relu',input_shape=(250,250,1),padding='same'))
+        #detection_model.add(LeakyReLU(alpha=0.1))
         detection_model.add(MaxPooling2D((2,2),padding='same'))
-        detection_model.add(Conv2D(64,(3,3),activation='linear',padding='same'))
-        detection_model.add(LeakyReLU(alpha=0.1))
+        #second layer
+        detection_model.add(Conv2D(64,(5,5),activation='relu',padding='same'))
+        #detection_model.add(LeakyReLU(alpha=0.1))
         detection_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
-        detection_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
-        detection_model.add(LeakyReLU(alpha=0.1))                  
+        #third layer
+        detection_model.add(Conv2D(128, (5, 5), activation='relu',padding='same'))
+        #detection_model.add(LeakyReLU(alpha=0.1))                  
         detection_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+        #flatten the output of the last pooling layer
         detection_model.add(Flatten())
-        detection_model.add(Dense(128, activation='linear'))
-        detection_model.add(LeakyReLU(alpha=0.1))
+        #fully connected layer
+        detection_model.add(Dense(128, activation='relu'))
+        #detection_model.add(LeakyReLU(alpha=0.1))
+
         detection_model.add(Dense(self.num_classes,activation='softmax')) 
         return detection_model                
 
